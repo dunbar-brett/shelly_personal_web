ed3505ad06daa3a72ef28e7963ab448074d2e079
Brett Dunbar <dunbar.brett@gmail.com>
2020-01-27 21:36:34 -0800

    change route names

--- layouts/partials/nav.vue
+++ layouts/partials/nav.vue
@@ -23,10 +23,10 @@
                         Watercolor
                     </b-nav-item>
                 </b-nav-item-dropdown> -->
-                <b-nav-item :to="'/oils-gallery'" class="sw-nav-link">
+                <b-nav-item :to="'/oils'" class="sw-nav-link">
                     Oil
                 </b-nav-item>
-                <b-nav-item :to="'/watercolor-gallery'" class="sw-nav-link">
+                <b-nav-item :to="'/watercolor'" class="sw-nav-link">
                     Watercolor
                 </b-nav-item>
                 <b-nav-item :to="'/about'" class="sw-nav-link">
